0bcbb7c8c7cb7df889f834883165b9d787e1a772
G-orwell <orwellgeorge55@gmail.com>
2026-04-27 14:43:10 +0300

    Update app.py
    update make to recall itself

--- app.py
+++ app.py
@@ -350,7 +350,7 @@ class ThreadSafeFileProcessor:
             utc_time_now = int( float(time.time()) )
             if (self.last_time_run is None) or (utc_time_now - self.last_time_run >= self.INTERVAL ):
                 self.last_time_run = utc_time_now
-                yield from yieldString("url::mg/web_page/me.html/processpage?yield_to_remote=1&_server{_server}")
+                yield from yieldString(f"url::mg/web_page/me.html/processpage?yield_to_remote=1&_server{_server}")
                 # yield from yieldString("url::mg/pythonanywhere_api/talentors2/webapps")
                 # yield from yieldString("url::mg/pythonanywhere_api/talentors2/update")
                 # update last run

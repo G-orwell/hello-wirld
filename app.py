fdbdcb2af1ac25923ad8c1c0a682cae50085c385
G-orwell <orwellgeorge55@gmail.com>
2026-06-19 15:45:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket , WebSocketDisconnect
 import os
 
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        print("Client connected:", len(self.active_connections))
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+        print("Client disconnected:", len(self.active_connections))
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
 app = FastAPI()
 socket_app = app
 
 @app.websocket("/ws")
 async def ws_endpoint(ws: WebSocket):
-    await ws.accept()
+    await manager.connect(websocket)
     print("WebSocket connected")
 
     try:
         while True:
             data = await ws.receive_text()
             print("MESSAGE RECEIVED:", data)
+
+            # broadcast to ALL clients
+            await manager.broadcast(data)
     except Exception as e:
+        manager.disconnect(websocket)
         print("DISCONNECTED:", e)
 
 # HTTP endpoint
